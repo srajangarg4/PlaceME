d035ea9b80a615235ce7186fe9940804238a8ffb
srajangarg4 <41852512+srajangarg4@users.noreply.github.com>
2021-05-30 12:50:19 +0530

    Delete Icon.js

--- src/components/Icon.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Icon = ({ name, className }) => {
-  return (
-    <span className={`material-icons mx-1 ${className ?? ''}`}>{name}</span>
-  );
-};
-
-export default Icon;
